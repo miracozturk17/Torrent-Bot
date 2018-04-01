9be729845ed24763866b371b2c54e0845b4de2b0
Miraç  ÖZTÜRK <32125234+miracozturk17@users.noreply.github.com>
2018-04-02 00:10:42 +0300

    Torrent Bot

---
+++ Bot_App/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace Bot_App
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtseach = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnsearch = new System.Windows.Forms.Button();
+            this.lstitem = new System.Windows.Forms.ListView();
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // txtseach
+            // 
+            this.txtseach.Location = new System.Drawing.Point(298, 28);
+            this.txtseach.Name = "txtseach";
+            this.txtseach.Size = new System.Drawing.Size(273, 20);
+            this.txtseach.TabIndex = 0;
+            this.txtseach.Text = "Lütfen bir iççerik giriniz...";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(28, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(160, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "ARAMAK ISTEDIGINIZ ICERIK:";
+            // 
+            // btnsearch
+            // 
+            this.btnsearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnsearch.Location = new System.Drawing.Point(298, 73);
+            this.btnsearch.Name = "btnsearch";
+            this.btnsearch.Size = new System.Drawing.Size(273, 35);
+            this.btnsearch.TabIndex = 2;
+            this.btnsearch.Text = "ARAMA YAP";
+            this.btnsearch.UseVisualStyleBackColor = true;
+            this.btnsearch.Click += new System.EventHandler(this.btnsearch_Click);
+            // 
+            // lstitem
+            // 
+            this.lstitem.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader3,
+            this.columnHeader4});
+            this.lstitem.FullRowSelect = true;
+            this.lstitem.GridLines = true;
+            this.lstitem.Location = new System.Drawing.Point(31, 134);
+            this.lstitem.Name = "lstitem";
+            this.lstitem.Size = new System.Drawing.Size(540, 292);
+            this.lstitem.TabIndex = 3;
+            this.lstitem.UseCompatibleStateImageBehavior = false;
+            this.lstitem.View = System.Windows.Forms.View.Details;
+            this.lstitem.SelectedIndexChanged += new System.EventHandler(this.lstitem_SelectedIndexChanged);
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "TITLE";
+            this.columnHeader3.Width = 150;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "ITEM LINK";
+            this.columnHeader4.Width = 250;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(634, 438);
+            this.Controls.Add(this.lstitem);
+            this.Controls.Add(this.btnsearch);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtseach);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtseach;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnsearch;
+        private System.Windows.Forms.ListView lstitem;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+    }
+}
+
---
+++ Bot_App/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows.Forms;
+using System.IO;
+using System.Net;
+using System.Text.RegularExpressions;
+
+namespace Bot_App
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Search(string keyword)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create("http://torrentz.eu/search?f=" + keyword);
+
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            StreamReader sr = new StreamReader(response.GetResponseStream());
+
+            string response_text = sr.ReadToEnd();
+
+            Regex regex = new Regex("<dt>.*?</dt>");
+
+            MatchCollection match = regex.Matches(response_text);
+
+            foreach (Match ping in match)
+            {
+
+                //MessageBox.Show(ping.Value);
+                //Clipboard.SetText(ping.Value);
+
+                Regex extract_link_regex = new Regex(@"href=""/.*""");
+                Match extract_link_match = extract_link_regex.Match((ping.Value));
+                string link = extract_link_match.Value;
+                link = link.Replace("href=", "");
+                link = link.Replace(@"""", "");
+                link = "http://arenabg.ch/" + link;
+
+
+                Regex extract_title_regex = new Regex(@""">.*</a>");
+                Match extract_title_match = extract_title_regex.Match((ping.Value));
+                string title = extract_title_match.Value;
+                title = title.Replace(@""">", "");
+                title = title.Replace(@"<b>", "");
+                title = title.Replace(@"</b>", "");
+                title = title.Replace(@"</a>", "");
+
+
+                ListViewItem items = new ListViewItem(new string[] { title, link });
+                items.Tag = link;
+                lstitem.Items.Add(items);
+            }
+        }
+
+        private void btnsearch_Click(object sender, EventArgs e)
+        {
+            Search(txtseach.Text);
+        }
+
+        private void lstitem_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                //IF YOU STARTED DIFFERENT EXPLORER WRİTE NAME !!!
+                Process.Start("chrome.exe", lstitem.SelectedItems[0].Tag.ToString());
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception);
+                throw;
+            }
+        }
+    }
+}
